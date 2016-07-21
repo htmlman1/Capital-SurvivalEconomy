a2bdff1ff7920092ea455962432413f1d4588bfd
CeramicTitan <ceramictitandev@gmail.com>
2016-07-21 14:52:43 -0600

    Adding signs to list

--- src/com/htmlman1/capitaleconomy/shop/CapitalShop.java
+++ src/com/htmlman1/capitaleconomy/shop/CapitalShop.java
@@ -1,23 +1,29 @@
 package com.htmlman1.capitaleconomy.shop;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.htmlman1.capitaleconomy.util.Serialization;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Sign;
 import org.bukkit.configuration.ConfigurationSection;
 
 import com.htmlman1.capitaleconomy.user.CapitalUser;
 
 public class CapitalShop {
-	
+
 	private CapitalUser owner;
-	private List<ShopSign> signs;
-	
+	private List<ShopSign> signs = new ArrayList<ShopSign>();//Fix NPE
+	ConfigurationSection s;
+
 	public CapitalShop(ConfigurationSection s) {
-		
+		this.s = s;
 	}
 
 	public CapitalShop(UUID owner, List<ShopSign> signs) {
-		
+
 	}
 
 	public CapitalUser getOwner() {
@@ -35,5 +41,10 @@ public class CapitalShop {
 	public void setSigns(List<ShopSign> signs) {
 		this.signs = signs;
 	}
-	
+
+	public void init() {
+        for(String locString : s.getStringList("Locations")){
+            getSigns().add(new ShopSign((Sign)Serialization.fromLocationString(locString).getBlock().getState()));
+        }
+	}
 }
